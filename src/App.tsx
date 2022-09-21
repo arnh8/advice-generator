5754a83d634e60ef3a4cae29fd105dd8c79ac1d7
Arnold <arnold.hsieh8@gmail.com>
2022-09-21 01:38:43 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import divider from "./assets/pattern-divider-desktop.svg";
+import dice from "./assets/icon-dice.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const url = "https://api.adviceslip.com/advice";
+
+    const [adviceStr, setAdviceStr] = useState("");
+    const [adviceNum, setAdviceNum] = useState(0);
+
+    async function fetchData() {
+        try {
+            const response = await fetch(url);
+            const json = await response.json();
+
+            setAdviceStr(json.slip.advice);
+            setAdviceNum(json.slip.id);
+        } catch (error) {
+            console.log("error", error);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <div className="App">
+            <div className="adviceNum">Advice #{adviceNum}</div>
+            <div className="adviceStr">"{adviceStr}"</div>
+            <img src={divider} alt="divider" />
+            <button className="adviceButton" onClick={fetchData}>
+                <img src={dice} alt="get-advice" />
+            </button>
+        </div>
+    );
 }
 
 export default App;
